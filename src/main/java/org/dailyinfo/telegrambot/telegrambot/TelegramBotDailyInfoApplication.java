fd11d7a01d87e9ef4fd3dcb431d8fd9e709ba5f8
Usenko Roman <basist92@gmail.com>
2021-02-26 19:00:49 +0200

    Initial commit

---
+++ src/main/java/org/dailyinfo/telegrambot/telegrambot/TelegramBotDailyInfoApplication.java
@@ -0,0 +1,13 @@
+package org.dailyinfo.telegrambot.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotDailyInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotDailyInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dailyinfo/telegrambot/telegrambot/TelegramBotDailyInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dailyinfo.telegrambot.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotDailyInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
