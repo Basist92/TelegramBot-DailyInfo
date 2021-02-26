bd586e33302bc634a638d6552ca61a676144051a
Usenko Roman <basist92@gmail.com>
2021-02-26 23:02:49 +0200

    have done aTemplate forTelegramBot remove application.properties from .gitignore

--- src/main/java/org/dailyinfo/telegrambot/telegrambot/service/TelegramBot.java
+++ src/main/java/org/dailyinfo/telegrambot/telegrambot/service/TelegramBot.java
@@ -1,32 +1,45 @@
 package org.dailyinfo.telegrambot.telegrambot.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.io.File;
+
 @Component
 @PropertySource("application.properties")
 public class TelegramBot extends TelegramLongPollingBot {
-    @Value("${bot.username}")
-    private String botUserName;
 
-    @Value("${bot.username}")
+    @Value("${bot.userName}")
+    private String botUserName;
+    @Value("${bot.token}")
     private String botToken;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        try {
+            objectMapper.writeValue(new File
+                    ("src/test/resources/update.json"),update);
+//            System.out.println("written");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return botUserName;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return botToken;
     }
 }
