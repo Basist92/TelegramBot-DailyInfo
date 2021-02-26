f93c50908cbba7d5e2b593bec2da0c3719fb4e4a
Usenko Roman <basist92@gmail.com>
2021-02-26 21:19:32 +0200

    have done TelegramBot as a component. And initialize an api

--- src/main/java/org/dailyinfo/telegrambot/telegrambot/service/TelegramBot.java
+++ src/main/java/org/dailyinfo/telegrambot/telegrambot/service/TelegramBot.java
@@ -2,9 +2,11 @@ package org.dailyinfo.telegrambot.telegrambot.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Component
 @PropertySource("application.properties")
 public class TelegramBot extends TelegramLongPollingBot {
     @Value("${bot.username}")
