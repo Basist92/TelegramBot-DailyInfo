b9f12d3f3e1f06b73f9f6cf721d0c3b06cdd0c6f
Usenko Roman <basist92@gmail.com>
2021-02-27 05:32:09 +0200

    update method TelegramBot.onUpdateReceived

--- src/main/java/org/dailyinfo/telegrambot/telegrambot/service/TelegramBot.java
+++ src/main/java/org/dailyinfo/telegrambot/telegrambot/service/TelegramBot.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
 
@@ -20,15 +22,22 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Autowired
     ObjectMapper objectMapper;
+    @Autowired
+    MessageService messageService;
 
     @Override
     public void onUpdateReceived(Update update) {
+
+//      this is method will need for covering by test (he saves JSON)
+//        saveJSON(update);
+
+        SendMessage sendMessage = messageService.onUpdateReceived(update);
         try {
-            objectMapper.writeValue(new File("src/test/resources/update.json"),update);
-//            System.out.println("written");
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
+
     }
 
     @Override
@@ -39,5 +48,13 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return botToken;
+    }
+
+    private void saveJSON(Update update) {
+        try {
+            objectMapper.writeValue(new File("src/test/resources/update.json"), update);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
