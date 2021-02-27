221406c847d18018d3d7f004282cee3ecc5d61fc
Usenko Roman <basist92@gmail.com>
2021-02-27 05:08:00 +0200

    create method to sending message. Covered it by tests

--- src/test/java/org/dailyinfo/telegrambot/telegrambot/service/MessageServiceTest.java
+++ src/test/java/org/dailyinfo/telegrambot/telegrambot/service/MessageServiceTest.java
@@ -6,7 +6,9 @@ import org.dailyinfo.telegrambot.telegrambot.config.Mapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -25,8 +27,8 @@ class MessageServiceTest {
     void onUnknownReceived() throws IOException {
         Update update = objectMapper.readValue(new File("src/test/resources/update.json"), Update.class);
 
-        String actualResult = messageService.onUpdateReceived(update);
-        String expectedResult = "sorry, i don`t understand you";
+        SendMessage actualResult = messageService.onUpdateReceived(update);
+        SendMessage expectedResult = createMessage("sorry, i don`t understand you");
         assertEquals(expectedResult, actualResult);
     }
 
@@ -36,8 +38,8 @@ class MessageServiceTest {
         Update update = objectMapper.readValue(
                 new File("src/test/resources/botCommand/start.json"), Update.class);
 
-        String actualResult = messageService.onUpdateReceived(update);
-        String expectedResult = "Hi! I am your bot for every day =)";
+        SendMessage actualResult = messageService.onUpdateReceived(update);
+        SendMessage expectedResult = createMessage("Hi! I am your bot for every day =)");
         assertEquals(expectedResult, actualResult);
     }
 
@@ -47,8 +49,8 @@ class MessageServiceTest {
         Update update = objectMapper.readValue(
                 new File("src/test/resources/botCommand/help.json"), Update.class);
 
-        String actualResult = messageService.onUpdateReceived(update);
-        String expectedResult = "assistance at the development stage";
+        SendMessage actualResult = messageService.onUpdateReceived(update);
+        SendMessage expectedResult = createMessage("assistance at the development stage");
         assertEquals(expectedResult, actualResult);
     }
 
@@ -58,8 +60,15 @@ class MessageServiceTest {
         Update update = objectMapper.readValue(
                 new File("src/test/resources/botCommand/settings.json"), Update.class);
 
-        String actualResult = messageService.onUpdateReceived(update);
-        String expectedResult = "currently no settings";
+        SendMessage actualResult = messageService.onUpdateReceived(update);
+        SendMessage expectedResult = createMessage("currently no settings");
         assertEquals(expectedResult, actualResult);
+    }
+
+    private SendMessage createMessage(String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(763001640L);
+        sendMessage.setText(text);
+        return sendMessage;
     }
 }
--- src/test/java/org/dailyinfo/telegrambot/telegrambot/service/TelegramBotTest.java
+++ src/test/java/org/dailyinfo/telegrambot/telegrambot/service/TelegramBotTest.java
@@ -5,7 +5,9 @@ import org.dailyinfo.telegrambot.telegrambot.config.Mapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,6 +26,14 @@ class TelegramBotTest {
     void onUpdateReceived() throws IOException {
         Update update = objectMapper.readValue(new File("src/test/resources/update.json"), Update.class);
         telegramBot.onUpdateReceived(update);
+    }
+
+    @Test
+    void sendMessage() throws TelegramApiException {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(763001640L);
+        sendMessage.setText("Thank you");
+        telegramBot.execute(sendMessage);
     }
 
 
