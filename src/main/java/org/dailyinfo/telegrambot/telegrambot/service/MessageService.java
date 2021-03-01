adf43b9022580dea84ee05e0e5ad9ec8211ac42c
DenisAbramovDm <sdgnhfs@gmail.com>
2021-03-01 22:27:33 +0300

    added weather

--- src/main/java/org/dailyinfo/telegrambot/telegrambot/service/MessageService.java
+++ src/main/java/org/dailyinfo/telegrambot/telegrambot/service/MessageService.java
@@ -1,15 +1,19 @@
 package org.dailyinfo.telegrambot.telegrambot.service;
 
+import org.dailyinfo.telegrambot.telegrambot.service.weather.Weather;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.io.IOException;
 
 @Service
 public class MessageService {
 
     public SendMessage onUpdateReceived(Update update) {
         SendMessage sendMessage = new SendMessage();
+        Weather weather = new Weather();
         if (update != null) {
             Message message = update.getMessage();
             sendMessage.setChatId(message.getChatId());
@@ -21,6 +25,12 @@ public class MessageService {
                     return sendMessage.setText("currently no settings");
                 } else if (messageText.equals("/help")) {
                     return sendMessage.setText("assistance at the development stage");
+                }else if (messageText.equals("/weather")) {
+                    try {
+                        return sendMessage.setText(weather.connection());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
--- src/main/java/org/dailyinfo/telegrambot/telegrambot/service/weather/Weather.java
+++ src/main/java/org/dailyinfo/telegrambot/telegrambot/service/weather/Weather.java
@@ -1,4 +1,48 @@
 package org.dailyinfo.telegrambot.telegrambot.service.weather;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+//"http://api.openweathermap.org/data/2.5/weather?q=London&appid=19efcf206130c164112f3620a141d3d1"
 public class Weather {
+    @Autowired
+    ObjectMapper objectMapper;
+    private void saveJSON(String mess) {
+        try {
+            objectMapper.writeValue(new File("src/main/resources/weather.json"), mess);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    public String connection() throws IOException {
+        final URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&lang=ru&appid=19efcf206130c164112f3620a141d3d1");
+        final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setConnectTimeout(1000);
+        con.setReadTimeout(1000);
+        try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            final StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            saveJSON(content.toString());
+            return content.toString();
+        } catch (final Exception ex) {
+            ex.printStackTrace();
+            return "";
+        }
+
+
+    }
+
+
 }
